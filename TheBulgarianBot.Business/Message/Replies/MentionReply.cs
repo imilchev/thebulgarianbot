81b02c94d7c8f71b4771471aa252b090dd4661fb
Ivan Milchev <ivan.milchev321@gmail.com>
2020-03-19 09:40:25 +0100

    fixed a bug in cursing other people

--- TheBulgarianBot.Business/Message/Replies/MentionReply.cs
+++ TheBulgarianBot.Business/Message/Replies/MentionReply.cs
@@ -77,7 +77,7 @@
 
             if (entity != null)
             {
-                return entity.User.Username.Equals(username, StringComparison.OrdinalIgnoreCase);
+                return !(entity.User.Username == null) && entity.User.Username.Equals(username, StringComparison.OrdinalIgnoreCase);
             }
 
             var text = message.Text;
